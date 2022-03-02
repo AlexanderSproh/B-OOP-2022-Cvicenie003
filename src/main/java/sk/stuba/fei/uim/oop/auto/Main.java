5226c464443b6becfbf389d01d2e5b09b92eccd3
Sale PC <xsproh@stuba.sk>
2022-03-02 16:29:21 +0100

    Auto

---
+++ src/main/java/sk/stuba/fei/uim/oop/auto/Main.java
@@ -0,0 +1,21 @@
+package sk.stuba.fei.uim.oop.auto;
+
+import sk.stuba.fei.uim.oop.auto.auto.Auto;
+
+public class Main {
+    public static void main(String[] args) {
+        Auto skoda = new Auto();
+        Auto vw = new Auto(50);
+
+        System.out.println("skoda");
+        System.out.println(skoda.stav());
+        skoda.jazdi(100);
+        System.out.println(skoda.stav());
+
+        System.out.println("vw");
+        System.out.println(vw.stav());
+        vw.jazdi(100);
+        System.out.println(vw.stav());
+
+    }
+}
